56f74425ec0413f3b5b0d8f25500f33ef7bd9b92
Janan A <jananakkad@hotmail.co.uk>
2018-10-29 22:03:10 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,3 @@
-001:0 > def display_board
-002:1 >     puts "A Tic Tac Toe Board"
-003:1 >   end
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+def display_board
+  puts "A Tic Tac Toe Board"
+end
